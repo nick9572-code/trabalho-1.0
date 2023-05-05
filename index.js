5e6e4494dcceaf403e1dec677077fd65244fd039
seilasaporra <equevedo724@gmail.com>
2023-05-05 16:47:14 -0300

    hjsa]]

---
+++ index.js
@@ -0,0 +1,89 @@
+//    function somarnumero(n1)
+//    {
+//     const calculo = n1 * 5
+//     return calculo
+//    }
+//    console.log(somarnumero(2))
+//    console.log(somarnumero(10))
+
+// // o codigo ira multiplicaer os numeros do console
+// // se retirarmos o console log nao ira rodar o codigo 
+
+
+//    let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) 
+// {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+// // Esse codigo indentifica a palavra cenoura
+
+
+
+
+// const nome = prompt("qual seu nome?")
+// const numero = prompt("qual sua idade?")
+// const cidade = prompt("qual a cidade onde você mora?")  
+// const profissao = prompt("qual a sua profissão?")
+
+// console.log(´Eu sou ${nome), tenho ${numero) anos, moro em ${cidade) e sou ${profissão)´)
+// let TextoDoUsuario = prompt ("insira um texto")
+
+// const outraFuncao = function(texto)
+// {
+//     return texto.toLowerCase().includes ("cenoura")
+// }
+
+// const resposta = outraFuncao (TextoDoUsuario)
+// console.log(resposta)
+
+
+
+
+// function soma(n1,n2)
+// {
+//     return n1 + n2
+// }
+
+// function verinumero(n1,n2)
+// {
+//     return n1 >= n2
+// }
+
+// console.log(soma(80,20))
+// console.log(verinumero(80,20))
+
+
+
+
+// function operadores(numero1,numero2)
+// {
+//     const soma = numero1 + numero2
+//     const subraçao = numero1 - numero2
+//     const multiplicar = numero1 * numero2
+//     const divisao = numero1 / numero2
+// }
+
+// const numero1 = Number(prompt("Digite um numero"))
+// const numero2 = Number(prompt("Digite outro numero"))
+// console.log(calculadora(numero1,numero2));
+
+
+
+
+// const imprimir = (parametro) => 
+// {
+//     console.log(parametro)
+// }
+//  const somatorio = (n1,n2) => 
+//  {
+//     const soma = n1 + n2
+//     imprimir(soma)
+//  }
+
+//  somatorio(35,34)
+
